3fb9db7649a3c667675bd473d182ed2ffc584abd
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-04-08 23:50:22 +0600

    Data Fetch is done

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+    $connection = new mysqli('localhost', 'root', '', 'school_management_system_16'); //true
+
+    // if($connection == true){
+    //     echo "Connected Successfully!";
+    // }
+    // else{
+    //     echo "Connection Failed!";
+    // }
+
+?>
